f3cad1db53d3bd9fb7bc1e5c3e05b3c5eaa3f256
fraicheveracruz <proveedores@fraicheveracruz.com.mx>
2018-12-06 10:50:09 -0600

    Updates

---
+++ precache-manifest.e5af0d7a01a7be7b20fb7aaae221c625.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d918d92dd23e45beaede7eb495a3f75",
+    "url": "/fraiche/static/media/coche.8d918d92.jpg"
+  },
+  {
+    "revision": "c15d062b1be26f12aba0",
+    "url": "/fraiche/static/css/main.32da05d4.chunk.css"
+  },
+  {
+    "revision": "173b37f9155bea296ef0",
+    "url": "/fraiche/static/js/1.173b37f9.chunk.js"
+  },
+  {
+    "revision": "1af8aaa44bb5628ed109",
+    "url": "/fraiche/static/js/runtime~main.1af8aaa4.js"
+  },
+  {
+    "revision": "ebac230fd02aa3d9f5fd92b59c602486",
+    "url": "/fraiche/static/media/rollOn.ebac230f.jpg"
+  },
+  {
+    "revision": "a236c66d8c84569c16fb4380b9466d10",
+    "url": "/fraiche/static/media/softSkin.a236c66d.jpg"
+  },
+  {
+    "revision": "7548c61905d4cc9adbf38eb9979060fd",
+    "url": "/fraiche/static/media/helloKitty.7548c619.jpg"
+  },
+  {
+    "revision": "c15d062b1be26f12aba0",
+    "url": "/fraiche/static/js/main.c15d062b.chunk.js"
+  },
+  {
+    "revision": "6d855d2abcd1d98232a75ae5ef5e09ae",
+    "url": "/fraiche/static/media/aromatizante.6d855d2a.jpg"
+  },
+  {
+    "revision": "a740d0cc4a980db8f93c14f2aa900282",
+    "url": "/fraiche/static/media/axel.a740d0cc.jpg"
+  },
+  {
+    "revision": "28a1f10184fb7cc35f62e428ab6e4da8",
+    "url": "/fraiche/static/media/spraySecrets.28a1f101.jpg"
+  },
+  {
+    "revision": "bc4db414e728e59c9031f656002e2315",
+    "url": "/fraiche/static/media/perfumeros.bc4db414.jpg"
+  },
+  {
+    "revision": "8d9af7670e68e771190260a2e8a59c48",
+    "url": "/fraiche/static/media/liliana.8d9af767.jpg"
+  },
+  {
+    "revision": "f28856c5fcef765325ef8486d7d8567e",
+    "url": "/fraiche/static/media/transparent_logo.f28856c5.png"
+  },
+  {
+    "revision": "29c72dfbaa54ef3ef2b687d03d1c88b0",
+    "url": "/fraiche/index.html"
+  }
+];
